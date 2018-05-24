9c87ae1ac54a74830a19bfd1a2d00e8dddbf86c4
DESKTOP-M3MTBIK\otomi <olah.tamas@fits.hu>
2018-05-24 19:56:22 +0200

    drawing circle and center point to layout

--- app/src/main/java/com/example/tmartin/bluetooth_duino/LedControl.kt
+++ app/src/main/java/com/example/tmartin/bluetooth_duino/LedControl.kt
@@ -8,7 +8,10 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_led_control.*
 import java.io.IOException
 import java.io.InputStream
@@ -32,6 +35,10 @@ class LedControl : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_led_control)
 
+        val layout1 = findViewById(R.id.layout) as android.support.constraint.ConstraintLayout
+        val background = Canvass(this)
+        layout1.addView(background)
+
         mAddress = intent.getStringExtra(DeviceListActivity.EXTRA_ADDRESS)
         ConnectToDevice(this).execute()
         control_forward.setOnClickListener { sendCommand("f") }
@@ -40,6 +47,25 @@ class LedControl : AppCompatActivity() {
         control_back.setOnClickListener { sendCommand("b") }
         control_led_disconnect.setOnClickListener { disconnect() }
     }
+
+    class Canvass (context: Context): View(context) {
+
+        override fun onDraw (canvas: Canvas) {
+            canvas.drawRGB (255, 255, 255)
+            val width = getWidth ()
+            val hieght = getHeight ()
+            val brush1 = Paint ()
+            val brush2 = Paint()
+            brush1.setARGB (255, 0, 0, 0)
+            brush2.setARGB(255, 255, 0, 0)
+            brush1.setStyle (Paint.Style.STROKE)
+
+            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(),5.toFloat(), brush2)
+            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(), 340.toFloat(), brush1)
+
+        }
+    }
+
 
     private fun sendCommand(input: String) {
         Log.d("LedControl", "sendCommand, inpu: ${input}")
