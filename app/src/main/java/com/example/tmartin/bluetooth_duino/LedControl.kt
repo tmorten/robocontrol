99fc39bbd4aced2d10f0c6226fd14ab08d8c085d
DESKTOP-M3MTBIK\otomi <olah.tamas@fits.hu>
2018-05-24 20:09:24 +0200

    refactor

--- app/src/main/java/com/example/tmartin/bluetooth_duino/LedControl.kt
+++ app/src/main/java/com/example/tmartin/bluetooth_duino/LedControl.kt
@@ -35,9 +35,9 @@ class LedControl : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_led_control)
 
-        val layout1 = findViewById(R.id.layout) as android.support.constraint.ConstraintLayout
-        val background = Canvass(this)
-        layout1.addView(background)
+        val _layout = findViewById(R.id.layout) as android.support.constraint.ConstraintLayout
+        val background = CreateCanvas(this)
+        _layout.addView(background)
 
         mAddress = intent.getStringExtra(DeviceListActivity.EXTRA_ADDRESS)
         ConnectToDevice(this).execute()
@@ -48,21 +48,20 @@ class LedControl : AppCompatActivity() {
         control_led_disconnect.setOnClickListener { disconnect() }
     }
 
-    class Canvass (context: Context): View(context) {
+    class CreateCanvas (context: Context): View(context) {
 
         override fun onDraw (canvas: Canvas) {
             canvas.drawRGB (255, 255, 255)
             val width = getWidth ()
             val hieght = getHeight ()
-            val brush1 = Paint ()
-            val brush2 = Paint()
-            brush1.setARGB (255, 0, 0, 0)
-            brush2.setARGB(255, 255, 0, 0)
-            brush1.setStyle (Paint.Style.STROKE)
+            val brushRadar = Paint ()
+            val brushCenter = Paint()
+            brushRadar.setARGB (255, 0, 0, 0)
+            brushCenter.setARGB(255, 255, 0, 0)
+            brushRadar.setStyle (Paint.Style.STROKE)
 
-            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(),5.toFloat(), brush2)
-            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(), 340.toFloat(), brush1)
-
+            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(),5.toFloat(), brushCenter)
+            canvas.drawCircle((width/2).toFloat(), (height - (height/3)).toFloat(), 340.toFloat(), brushRadar)
         }
     }
 
